65d1bc783ddb1a50258eea0aa516d3e516b2183a
rakshithvasudev <rakshithvbharani@gmail.com>
2023-09-02 22:48:11 -0500

    enable swiglu non linearity setup

--- run.c
+++ run.c
@@ -274,6 +274,7 @@ void matmul(float *xout, float *x, float *w, int n, int d) {
 }
 
 float *forward(Transformer *transformer, int token, int pos) {
+
   // convienence variables
   Config *p = &transformer->config;
   TransformerWeights *w = &transformer->weights;
@@ -292,9 +293,100 @@ float *forward(Transformer *transformer, int token, int pos) {
   memcpy(x, content_row, dim * sizeof(*x));
 
   // forward all layers
-  for (int l = 0; l < p->n_layers; l++) {
+  for (unsigned long long l = 0; l < p->n_layers; l++) {
+    
+    // attention rmsnorm
+    rmsnorm(s->xb, x, w->rms_att_weight + l * dim, dim);
 
-  }
-}
+    // qkv matmuls for this position
+    matmul(s->q, s->xb, w->wq + l * dim * dim, dim, dim);
+    matmul(s->k, s->xb, w->wk + l * dim * kv_dim, dim, kv_dim);
+    matmul(s->v, s->xb, w->wv + l * dim * kv_dim, dim, kv_dim);
 
-int main() { return 0; }
+    // RoPE relative pos encoding: complex-vlaued rotate q and k in each head
+    for (int i = 0; i < dim; i += 2) {
+      int head_dim = i % head_size;
+      float freq = 1.0f / powf(10000.0f, head_dim / (float)head_size);
+      float val = pos * freq;
+      float fcr = cosf(val);
+      float fci = sinf(val);
+      int rotn = i < kv_dim ? 2 : 1; // how many vectors? 2 = q & k, 1=q only
+      for (int v = 0; v < rotn; v++) {
+        float *vec =
+            v == 0 ? s->q : s->k; // the vector to rotate (query or key)
+        float v0 = vec[i];
+        float v1 = vec[i + 1];
+        vec[i] = v0 * fcr - v1 * fci;
+        vec[i + 1] = v0 * fci + v1 * fcr;
+      }
+    }
+    // save key, value at this time step (pos) to our kv cache
+    int loff = l * p->seq_len * kv_dim; // kv cache later offset for convenience
+    float *key_cache_row = s->key_cache + loff + pos * kv_dim;
+    float *value_cache_row = s->key_cache + loff + pos * kv_dim;
+    memcpy(key_cache_row, s->k, kv_dim * sizeof(*key_cache_row));
+    memcpy(value_cache_row, s->v, kv_dim * sizeof(*value_cache_row));
+
+    // multihead attn
+    int h;
+    #pragma omp parallel for private(h)
+    for (h = 0; h < p->n_heads; h++) {
+      // get the query vector for this head
+      float *q = s->q + h * head_size;
+      // attention scores for this head
+      float *att = s->att + h * p->seq_len;
+      // iterate over all timestamps, including the current one
+      for (int t = 0; t <= pos; t++) {
+        // get the key vector for this head and at this timestamp
+        float *k = s->key_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
+        // calcuate the attention score as the dot product of q and k
+        float score = 0.0f;
+        for (int i = 0; i < head_size; i++) {
+          score += q[i] * k[i];
+        }
+        score /= sqrtf(head_size);
+        // save the score to attention buffer
+        att[t] = score;
+      }
+
+      // softmax the scores to get attention weights, from 0..pos inclusively
+      softmax(att, pos + 1);
+
+      // weighted sum of values, store back into xb
+      float *xb = s->xb + h * head_size;
+      memset(xb, 0, head_size * sizeof(float));
+      for (int t = 0; t <= pos; t++) {
+        // get the value vector for this head and at this timestep
+        float *v =
+            s->value_cache + loff + t * kv_dim + (h / kv_mul) * head_size;
+        // get the attention weight for this timestep
+        float a = att[t];
+        // accumulate the weighted value into xb
+        for (int i = 0; i < head_size; i++) {
+          xb[i] += a * v[i];
+        }
+      }
+    }
+
+    // final matmul to get the output of attention
+    matmul(s->xb2, s->xb, w->wo + l * dim * dim, dim, dim);
+
+    // residual connection back into x
+    for (int i =0; i < dim; i++){
+    x[i] += s->xb2[i];
+   } 
+
+    //ffn rms
+    rmsnorm(s->xb, x, w->rms_ffn_weight + l * dim, dim);
+
+    // Now for FFN in Pytorch we have: self.w2(F.silu(self.w1(x))*self.w3(x))
+    // first calculate self.w1(x) and self.w3(x)
+    matmul(s->hb, s->xb, w->w1 + l * dim * hidden_dim, dim, hidden_dim);
+    matmul(s->hb2, s->xb, w->w3 + l * dim * hidden_dim, dim, hidden_dim);
+    
+    // SwiGLU non linearity 
+
+ 
+
+
+  int main() { return 0; }
