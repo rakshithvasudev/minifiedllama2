8b9813981f5e360e767f4046c2f6b5f2528aff2b
rakshithvasudev <rakshithvbharani@gmail.com>
2023-09-03 00:17:18 -0500

    enable lookup

--- run.c
+++ run.c
@@ -443,12 +443,12 @@ int compare_tokens(const void *a, const void *b) {
   return strcmp(((TokenIndex *)a)->str, ((TokenIndex *)b)->str);
 }
 
-void build_tokenizer(Tokenizer *t, char* tokenizer_path, int vocab_size) {
+void build_tokenizer(Tokenizer *t, char *tokenizer_path, int vocab_size) {
 
   // should have written vocab_size into the tokenizer file
   t->vocab_size = vocab_size;
   // malloc space to hold the scores and the strings
-  t->vocab = (char**)malloc(vocab_size * sizeof(char *));
+  t->vocab = (char **)malloc(vocab_size * sizeof(char *));
   t->vocab_scores = (float *)malloc(vocab_size * sizeof(char *));
   t->sorted_vocab = NULL; // lazy init
   for (int i = 0; i < 256; i++) {
@@ -456,45 +456,87 @@ void build_tokenizer(Tokenizer *t, char* tokenizer_path, int vocab_size) {
     t->byte_pieces[i * 2 + 1] = '\0';
   }
 
-// read in the file
-FILE *file = fopen(tokenizer_path, "rb");
-if (!file) {
-  fprintf(stderr, "Couldn't load %s \n", tokenizer_path);
-  exit(EXIT_FAILURE);
-}
-if (fread(&t->max_token_length, sizeof(int), 1, file) != 1) {
-  fprintf(stderr, "failed read\n");
-  exit(EXIT_FAILURE);
-}
-int len;
-for (int i = 0; i < vocab_size; i++) {
-  if (fread(t->vocab_scores + i, sizeof(float), 1, file) != 1) {
+  // read in the file
+  FILE *file = fopen(tokenizer_path, "rb");
+  if (!file) {
+    fprintf(stderr, "Couldn't load %s \n", tokenizer_path);
+    exit(EXIT_FAILURE);
+  }
+  if (fread(&t->max_token_length, sizeof(int), 1, file) != 1) {
     fprintf(stderr, "failed read\n");
     exit(EXIT_FAILURE);
   }
-  if (fread(&len, sizeof(int), 1, file) != 1) {
-    fprintf(stderr, "failed read\n");
-    exit(EXIT_FAILURE);
+  int len;
+  for (int i = 0; i < vocab_size; i++) {
+    if (fread(t->vocab_scores + i, sizeof(float), 1, file) != 1) {
+      fprintf(stderr, "failed read\n");
+      exit(EXIT_FAILURE);
+    }
+    if (fread(&len, sizeof(int), 1, file) != 1) {
+      fprintf(stderr, "failed read\n");
+      exit(EXIT_FAILURE);
+    }
+    t->vocab[i] = (char *)malloc(len + 1);
+    if (fread(t->vocab[i], len, 1, file) != 1) {
+      fprintf(stderr, "failed read\n");
+      exit(EXIT_FAILURE);
+    }
+    t->vocab[i][len] = '\0'; // add the string terminating token
   }
-  t->vocab[i] = (char *)malloc(len + 1);
-  if (fread(t->vocab[i], len, 1, file) != 1) {
-    fprintf(stderr, "failed read\n");
-    exit(EXIT_FAILURE);
-  }
-  t->vocab[i][len] = '\0'; // add the string terminating token
+  fclose(file);
 }
-fclose(file);
-}
-
 
 void free_tokenizer(Tokenizer *t) {
-  
-  for (int i = 0; i < t->vocab_size; i++) { free(t->vocab[i]);}
+
+  for (int i = 0; i < t->vocab_size; i++) {
+    free(t->vocab[i]);
+  }
   free(t->vocab);
   free(t->vocab_scores);
   free(t->sorted_vocab);
 }
 
-char* decode(Tokenizer* t, int prev_token, int token)
+char *decode(Tokenizer *t, int prev_token, int token) {
+
+  char *piece = t->vocab[token];
+  // following BOS (1) token, sentencepiece decoder strips any leading
+  // whitespace
+  if (prev_token == 1 && piece[0] == ' ') {
+    piece++;
+  }
+  // careful, some tokens designate raw bytes and look like ex: <0x01>
+  // parse this and convert and return the actual byte
+  unsigned char byte_val;
+  if (sscanf(piece, "<0x%02hhX>", &byte_val) == 1) {
+    piece = (char *)t->byte_pieces + byte_val * 2;
+  }
+  return piece;
+}
+
+
+void safe_printf(char *piece) {
+    // piece might be a raw byte token, and we only want to print printable chars or whitespace
+    // because some of the other bytes can be various control codes, backspace, etc.
+    if (piece == NULL) { return; }
+    if (piece[0] == '\0') { return; }
+    if (piece[1] == '\0') {
+        unsigned char byte_val = piece[0];
+        if (!(isprint(byte_val) || isspace(byte_val))) {
+            return; // bad byte, don't print it
+        }
+    }
+    printf("%s", piece);
+}
+
+int str_lookup(char *str, TokenIndex * sorted_vocab, int vocab_size){
+   // efficiently find the perfect match for str in vocab, return its index or -1 if not found
+   TokenIndex tok = {.str = str}; // acts as the key to search for 
+   TokenIndex *res = bsearch(&tok, sorted_vocab, vocab_size, sizeof(TokenIndex), compare_tokens);
+   return res!= NULL ? res->id: -1;
+}
+
+
+
+
 
 int main() { return 0; }
